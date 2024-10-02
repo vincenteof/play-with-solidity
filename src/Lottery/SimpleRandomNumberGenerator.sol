8bc1cef64ff16009ce516555b4d0adc2b07ac6fa
vincenteof <14847208+vincenteof@users.noreply.github.com>
2024-10-02 23:49:21 +0800

    chore: add chainlink

--- src/Lottery/SimpleRandomNumberGenerator.sol
+++ src/Lottery/SimpleRandomNumberGenerator.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.19;
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 import "../interfaces/IRandomNumberGenerator.sol";
-import "hardhat/console.sol";
 
 /**
  * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED VALUES FOR CLARITY.
