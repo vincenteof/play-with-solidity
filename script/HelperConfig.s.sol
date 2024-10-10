ac2d02aa921f1ec4150887927877f54de0f8f0cc
vincenteof <14847208+vincenteof@users.noreply.github.com>
2024-10-11 00:00:30 +0800

    fix: fund me test

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,13 +2,12 @@
 pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+struct NetworkConfig {
+    address priceFeed;
+}
 
 contract HelperConfig is Script {
-    struct NetworkConfig {
-        address priceFeed;
-    }
-
-    NetworkConfig public activeNetworkConfig;
+    NetworkConfig private activeNetworkConfig;
 
     constructor() {
         if (block.chainid == 11155111) {
@@ -18,26 +17,24 @@ contract HelperConfig is Script {
         }
     }
 
-    function getSepoliaEthConfig()
-        public
-        pure
-        returns (NetworkConfig memory networConfig)
-    {
-        networConfig = NetworkConfig({
-            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        });
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        return
+            NetworkConfig({
+                priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+            });
     }
 
-    function getAnvilEthConfig()
-        public
-        returns (NetworkConfig memory networConfig)
-    {
-        if (activeNetworkConfig.priceFeed == address(0)) {
+    function getAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
             return activeNetworkConfig;
         }
         vm.startBroadcast();
         MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 20000e8);
         vm.stopBroadcast();
-        networConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
+        return NetworkConfig({priceFeed: address(mockPriceFeed)});
+    }
+
+    function getActiveConfig() public view returns (NetworkConfig memory) {
+        return activeNetworkConfig;
     }
 }
--- script/fund-me/FundMe.s.sol
+++ script/fund-me/FundMe.s.sol
@@ -3,14 +3,15 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../../src/fund-me/FundMe.sol";
-import {HelperConfig} from "../HelperConfig.s.sol";
+import {HelperConfig, NetworkConfig} from "../HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
     function run() external returns (FundMe fundMe) {
         HelperConfig helperConfig = new HelperConfig();
-        (address priceFeed) = helperConfig.activeNetworkConfig();
+        // address priceFeed = helperConfig.activeNetworkConfig();
+        NetworkConfig memory config = helperConfig.getActiveConfig();
+        address priceFeed = config.priceFeed; 
         vm.startBroadcast();
-        // for Sepolia
         fundMe = new FundMe(priceFeed);
         vm.stopBroadcast();
     }
